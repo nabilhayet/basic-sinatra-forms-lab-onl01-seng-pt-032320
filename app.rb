1080b0e76abc58893886e6a1df0a1ff97e6d6398
Nabil Hayet <nabil.bracu@gmail.com>
2020-06-18 05:15:16 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @team_name_     = params[:team_name]
+    @coach_         = params[:coach]
+    @point_guard_   = params[:point]
+    @sho_guard_     = params[:sguard]
+    @sm_forward_    = params[:smforward]
+    @power_forward_ = params[:powforward]
+    @center_        = params[:center]
+
+    erb :team
+  end
+
 
 end
